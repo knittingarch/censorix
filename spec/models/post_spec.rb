d6bd4004484088b4894720858467facb64893f6d
Sarah Dawson <knittingarch@gmail.com>
2020-10-11 23:34:29 -0400

    Add Post model (#14)
    This commit adds the Post model.  Additional models that have an
    association with this model will be created in subsequent commits.

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:published_at) }
+    it { is_expected.to validate_presence_of(:title) }
+  end
+end
