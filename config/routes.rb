11185cf412b139f86388f5c39bf181115302c245
knittingarch <knittingarch@gmail.com>
2020-10-27 07:29:35 -0400

    Remove Category Index Page
    Now that the navigation between home page, blog lists by category, and
    individual posts have been established, there is no need for a dedicated
    category index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories, only: [:index, :show]
+  resources :categories, only: [:show]
   resources :posts, only: [:show]
 
   root to: "home#index"
