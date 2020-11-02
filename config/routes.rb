785328db7cfba7b3927d332e8661a9d32348f698
knittingarch <knittingarch@gmail.com>
2020-11-02 10:12:49 -0500

    add administrate with basic dashboards and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :categories
+      resources :tags
+      resources :posts
+
+      root to: "posts#index"
+    end
   resources :categories, only: [:show]
   resources :posts, only: [:show]
   resources :users, only: [:create, :new]
