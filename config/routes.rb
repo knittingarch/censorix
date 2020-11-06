3b31c3c938edbe6b3f87a35ae25f85e864bd7f31
knittingarch <knittingarch@gmail.com>
2020-11-06 12:01:06 -0500

    add users new / create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :categories, only: [:show]
   resources :posts, only: [:show]
+  resources :users, only: [:create, :new]
 
   root to: "home#index"
 end
