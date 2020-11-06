8d858922bb301cd361a3d6ae670ddc2a93bd5a27
knittingarch <knittingarch@gmail.com>
2020-11-06 13:36:05 -0500

    add authorization for individual page lock down

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
+  def authorize
+    redirect_to login_path, alert: "Please login" if current_user.nil?
+  end
+
   def current_user
     @current_user || User.find(session[:user_id]) if session[:user_id]
   end
