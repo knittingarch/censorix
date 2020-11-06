01efcbe05163cbe6b90b44aa2068a12113135e41
knittingarch <knittingarch@gmail.com>
2020-11-06 16:42:43 -0500

    remove has_many override and post dashboard show test

--- spec/system/admin_views_posts_dashboard_spec.rb
+++ spec/system/admin_views_posts_dashboard_spec.rb
@@ -1,23 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Admin views posts dasboard", type: :system do
-  # context "by clicking on sidebar link" do
-  #   it "shows the pertinent post data" do
-  #     category = create(:category, name: "Flowers")
-  #     tag = create(:tag, name: "gardening")
-  #     create(:post,
-  #       title: "Roses are red!",
-  #       categories: [category],
-  #       tags: [tag])
-
-  #     visit "/admin/posts"
-
-  #     expect(page).to have_content "Roses are red!"
-  #     expect(page).to have_content "Flowers"
-  #     expect(page).to have_content "gardening"
-  #   end
-  # end
-
+RSpec.describe "Admin views posts dashboard", type: :system do
   context "by clicking 'New post'" do
     it "leads to a page where a new post can be created" do
       create(:category, name: "Cooking")
